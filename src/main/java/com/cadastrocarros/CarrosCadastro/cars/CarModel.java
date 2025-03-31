2f8b2ca534d7e19076323c0e2d2f6c8a708b5d63
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-31 09:32:30 -0300

    feat: insercao de JPA e notations lombok

--- src/main/java/com/cadastrocarros/CarrosCadastro/cars/CarModel.java
+++ src/main/java/com/cadastrocarros/CarrosCadastro/cars/CarModel.java
@@ -1,27 +1,40 @@
 package com.cadastrocarros.CarrosCadastro.cars;
 
 import com.cadastrocarros.CarrosCadastro.cliente.ClienteModel;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Data
 @RequiredArgsConstructor
+@Entity
+@Table(name = "tb_carros")
 public class CarModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(name = "marca")
     String marca;
 
+    @Column(name = "modelo")
     String modelo;
 
+    @Column(name = "img_carro")
     String img;
 
+    @Column(name = "preco")
     Double price;
 
+    @Column(name = "chassi", unique = true)
     String chassi;
 
+    @Column(name = "cliente")
     ClienteModel cliente;
 
+    @Column(name = "placa", unique = true)
     String placa;
 
 
--- src/main/java/com/cadastrocarros/CarrosCadastro/cliente/ClienteModel.java
+++ src/main/java/com/cadastrocarros/CarrosCadastro/cliente/ClienteModel.java
@@ -1,21 +1,30 @@
 package com.cadastrocarros.CarrosCadastro.cliente;
 
 import com.cadastrocarros.CarrosCadastro.cars.CarModel;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@Entity
+@Table(name = "tb_cliente")
 public class ClienteModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(name = "nome")
     String nome;
 
+    @Column(name = "idade")
     int idade;
 
+    @Column(name = "cpf")
     String cpf;
 
+    @Column(name = "carro")
     CarModel car;
 
 
