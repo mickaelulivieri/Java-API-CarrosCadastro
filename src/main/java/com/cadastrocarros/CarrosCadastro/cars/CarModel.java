d1c946f5a16001908826209096573bc0203dcdd3
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-31 09:26:21 -0300

    refactor: utilizando lombok para simplificar codigo para depois inserir JPA

--- src/main/java/com/cadastrocarros/CarrosCadastro/cars/CarModel.java
+++ src/main/java/com/cadastrocarros/CarrosCadastro/cars/CarModel.java
@@ -1,8 +1,14 @@
 package com.cadastrocarros.CarrosCadastro.cars;
 
 import com.cadastrocarros.CarrosCadastro.cliente.ClienteModel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@RequiredArgsConstructor
 public class CarModel {
+
+    Long id;
 
     String marca;
 
@@ -18,64 +24,5 @@ public class CarModel {
 
     String placa;
 
-    public CarModel(String marca, String placa, ClienteModel cliente, String chassi, Double price, String img, String modelo) {
-        this.marca = marca;
-        this.placa = placa;
-        this.cliente = cliente;
-        this.chassi = chassi;
-        this.price = price;
-        this.img = img;
-        this.modelo = modelo;
-    }
 
-    public CarModel() {
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-
-    public String getChassi() {
-        return chassi;
-    }
-
-    public void setChassi(String chassi) {
-        this.chassi = chassi;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
 }
